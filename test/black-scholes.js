927c74f90cd546f712f4c54c73d82932a7ab6a25
MattL922 <mattloppatto@gmail.com>
2014-11-18 19:43:10 -0500

    removed epsilon var

--- test/black-scholes.js
+++ test/black-scholes.js
@@ -3,7 +3,6 @@ var assert = require("assert"),
 
 describe("Black-Scholes", function()
 {
-  var epsilon = 0.0001;
   it("should return a price of 0.23834902311961947", function()
   {
     assert.equal(0.23834902311961947, bs.blackScholes(30, 34, .25, .2, .08, "call"));
