f61a0f42c54203f02cd916c20d563f78cfb66781
MattL922 <mattloppatto@gmail.com>
2014-11-18 19:52:14 -0500

    added a test for a put price

--- test/black-scholes.js
+++ test/black-scholes.js
@@ -3,8 +3,12 @@ var assert = require("assert"),
 
 describe("Black-Scholes", function()
 {
-  it("should return a price of 0.23834902311961947", function()
+  it("should return a call price of 0.23834902311961947", function()
   {
     assert.equal(0.23834902311961947, bs.blackScholes(30, 34, .25, .2, .08, "call"));
+  });
+  it("should return a put price of 3.5651039155492974", function()
+  {
+    assert.equal(3.5651039155492974, bs.blackScholes(30, 34, .25, .2, .08, "put"));
   });
 });
